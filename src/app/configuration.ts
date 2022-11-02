bf0687d554777d44c87085c6d059b19a670b17a4
leliaflorina@gmail.com <leliaflorina@gmail.com>
2022-11-02 15:51:45 +0100

    bubbleb improuve styling

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -2,7 +2,7 @@ export const configuration = {
   header: {
     heading: 'BubbleBees NFTs',
     headingtext:
-      'A dinamic world of bubble bees:                                                                            ',
+      'A dinamic world of bubble bees                                                                            ',
     buttontext: 'join the hive',
     buttonlink: '\home'
   }
