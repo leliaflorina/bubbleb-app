30da4b982ef732d0efaed6662380613308f8e7bb
leliaflorina@gmail.com <leliaflorina@gmail.com>
2022-11-02 18:36:27 +0100

    improuvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { IntroComponent } from './intro/intro.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { ContentComponent } from './content/content.component';
-import { TeamComponent } from './team/team.component';
-import { FooterComponent } from './footer/footer.component';
-import { RoadmapComponent } from './roadmap/roadmap.component';
-import { HeaderComponent } from './header/header.component';
-import { SocialComponent } from './social/social.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ConfigService } from './config.service';
-import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
+import { AppComponent } from "./app.component";
+import { IntroComponent } from "./intro/intro.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { ContentComponent } from "./content/content.component";
+import { TeamComponent } from "./team/team.component";
+import { FooterComponent } from "./footer/footer.component";
+import { RoadmapComponent } from "./roadmap/roadmap.component";
+import { HeaderComponent } from "./header/header.component";
+import { SocialComponent } from "./social/social.component";
+import { NavigationComponent } from "./navigation/navigation.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { ConfigService } from "./config.service";
+import { NgxPageScrollCoreModule } from "ngx-page-scroll-core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [
@@ -26,12 +27,15 @@ import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
     RoadmapComponent,
     HeaderComponent,
     SocialComponent,
-    NavigationComponent
+    NavigationComponent,
   ],
-  imports: [BrowserModule,
-     AppRoutingModule,
-     NgxPageScrollCoreModule.forRoot({ duration: 500 })],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    NgxPageScrollCoreModule.forRoot({ duration: 500 }),
+  ],
   providers: [ConfigService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
