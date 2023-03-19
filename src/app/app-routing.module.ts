bb3e870f92d785ea36bfd8cdc5f151f13ec20fbe
leliaflorina@gmail.com <leliaflorina@gmail.com>
2023-03-19 18:59:15 +0100

    cleanup unused components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-import { IntroComponent } from './intro/intro.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { ContentComponent } from './content/content.component';
-import { TeamComponent } from './team/team.component';
-import { RoadmapComponent } from './roadmap/roadmap.component';
-import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/Home', pathMatch: 'full' },
-  { path: 'Home', component: HeaderComponent },
-  { path: 'About', component: IntroComponent },
-  { path: 'Services', component: ContentComponent },
-  { path: 'Team', component: TeamComponent },
-  { path: 'Gallery', component: GalleryComponent },
-  { path: 'Roadmap', component: RoadmapComponent },
-];
+  ];
 
 @NgModule({
   imports: [
