972e71127fb5ff0488c738002c751acb1df822b5
leliaflorina@gmail.com <leliaflorina@gmail.com>
2023-03-21 16:51:38 +0100

    bbs- fixed typo

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -2,7 +2,7 @@ export const configuration = {
   header: {
     heading: 'BubbleBees NFTs',
     headingtext:
-      'A dinamic world of bubble bees                                                                            ',
+      'A dynamic world of bubble bees                                                                            ',
     buttontext: 'join the hive',
     buttonlink: '\home'
   }
