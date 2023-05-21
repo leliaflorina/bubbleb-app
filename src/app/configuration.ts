100ff64107315704ab29e1e2ebbdfadc17c381cb
leliaflorina@gmail.com <leliaflorina@gmail.com>
2023-05-21 22:34:39 +0200

    changed save the bees page

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -2,7 +2,7 @@ export const configuration = {
   header: {
     heading: 'BubbleBees NFTs',
     headingtext:
-      'A dynamic world of bubble bees                                                                            ',
+      'A collection of digital artworks celebrating the vital role of bees in our ecosystem.                                                                          ',
     buttontext: 'join the hive',
     buttonlink: '\home'
   }
