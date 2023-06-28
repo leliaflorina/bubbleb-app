cdc8027077d672398921d0a754554e87c9346986
leliaflorina@gmail.com <leliaflorina@gmail.com>
2023-06-28 15:09:49 +0200

    bbs- improve title

--- src/app/configuration.ts
+++ src/app/configuration.ts
@@ -2,8 +2,8 @@ export const configuration = {
   header: {
     heading: 'BubbleBees NFTs',
     headingtext:
-      'A collection of digital artworks celebrating the vital role of bees in our ecosystem.                                                                          ',
-    buttontext: 'join the hive',
+      'Welcome to the enchanting world of BubbleBees. A collection of digital artworks celebrating the vital role of bees in our ecosystem.                                                                          ',
+    buttontext: 'Join the hive',
     buttonlink: '\home'
   }
 };
